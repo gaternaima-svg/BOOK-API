c2f57f0624b18fd94f81ac2b17424dfabfb439e7
Naima Gater <gaternaima@gmail.com>
2026-07-05 10:29:42 +0200

    Ajout de la route /books

--- app/main.py
+++ app/main.py
@@ -3,3 +3,11 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"message":"Bienvenue sur Book API"}
+@app.get("/books")
+def books():
+    return{
+        "books":[
+            {"id":1,"title":"python"},
+            {"id":2,"title":"FastAPI"}
+        ]
+    }
